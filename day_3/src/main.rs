33fa74539b71b371cde31b388095699a302802ed
Fergus Molloy <fdm504@york.ac.uk>
2020-12-03 10:48:03 +0000

    finished day 3

---
+++ day_3/src/main.rs
@@ -0,0 +1,47 @@
+use std::fs;
+
+//               X , Y
+struct Toboggan(usize, usize);
+
+fn main() {
+    let content = fs::read_to_string("input").expect("unable to read file");
+    let lines: Vec<&str> = content.lines()
+        .map(|x| x.trim())
+        .collect();
+
+    //part one
+    println!("trees encontered: {}", get_trees(&lines, 3, 1));
+    //part two
+    let result = get_trees(&lines, 1, 1)
+               * get_trees(&lines, 3, 1) 
+               * get_trees(&lines, 5, 1)
+               * get_trees(&lines, 7, 1)
+               * get_trees(&lines, 1, 2);
+    println!("result: {}", result);
+}
+
+fn get_trees(lines: &Vec<&str>, v_x: usize, v_y: usize) -> u32 {
+    let mut tob = Toboggan(0,0);
+    let mut trees = 0;
+
+    while tob.1 < (lines.len()-1) {
+        // get y coord
+        let line = lines.iter().nth(tob.1).unwrap();
+        // since patttern is repeating can use modulo to get index
+        let x = tob.0 % line.len();
+
+        if line.chars().nth(x).unwrap() == '#' {
+            trees += 1;
+        }
+
+        tob.0 += v_x;
+        tob.1 += v_y;
+    }
+    // just need to do the last line
+    let line = lines.iter().last().unwrap();
+    let x = tob.0 % line.len();
+    if line.chars().nth(x).unwrap() == '#' {
+        trees += 1;
+    }
+    trees
+}
