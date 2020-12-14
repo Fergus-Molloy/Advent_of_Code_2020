c3ebad10f35b5eca962c1c1baa90e88bd7db4f2d
Fergus Molloy <fdm504@york.ac.uk>
2020-12-14 21:47:00 +0000

    made it multithreaded but it is about 1000 times slower

--- day_5/benches/benchmark.rs
+++ day_5/benches/benchmark.rs
@@ -4,9 +4,10 @@ use std::fs;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let contents = fs::read_to_string("input").expect("unable to read file");
-    let lines: Vec<&str> = contents.lines().collect();
-    c.bench_function("part 1", |b| b.iter(|| part_one(&lines)));
-    c.bench_function("part 2", |b| b.iter(|| part_two(&lines)));
+    let lines: &Vec<String> = &contents.lines().map(|x| x.to_string()).collect();
+    let lines2 = lines.clone();
+    c.bench_function("part 1", |b| b.iter(|| part_one(lines)));
+    c.bench_function("part 2", |b| b.iter(|| part_two(&lines2)));
 }
 
 criterion_group!(benches, criterion_benchmark);
--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,28 +1,42 @@
 use std::fs;
+use std::thread;
 
 fn main() {
     let contents = fs::read_to_string("input").expect("unable to read file");
-    let lines: Vec<&str> = contents.lines().collect();
+    let lines: Vec<String> = contents.lines().map(|x| x.to_string()).collect();
+    let lines2 = lines.clone();
     println!("Part One: {}", part_one(&lines));
-    println!("Part Two: {}", part_two(&lines));
+
+    println!("Part Two: {}", part_two(&lines2));
 }
 
-pub fn part_one(lines: &Vec<&str>) -> u32 {
+pub fn part_one(lines: &Vec<String>) -> u32 {
     let mut big = 0;
     for line in lines.iter() {
-        let mut row: u8 = 127;
-        let mut iter = line.chars();
-        for x in (0..7).rev() {
-            if iter.next().unwrap() == 'F' {
-                row &= 127 - (2 as u8).pow(x);
+        let rows: Vec<char> = line.chars().take(7).collect();
+        let row_calc = thread::spawn(move || {
+            let mut row: u8 = 127;
+            for x in (0..7 as u32).rev() {
+                if rows.iter().nth(x as usize).unwrap() == &'F' {
+                    row &= 127 - (2 as u8).pow(x);
+                }
             }
-        }
-        let mut col: u8 = 7;
-        for x in (0..3).rev() {
-            if iter.next().unwrap() == 'L' {
-                col &= 7 - (2 as u8).pow(x);
+            row
+        });
+        let cols: Vec<char> = line.chars().take(3).collect();
+        let col_calc = thread::spawn(move || {
+            let mut col: u8 = 7;
+            for x in (0..3).rev() {
+                if cols.iter().nth(x as usize).unwrap() == &'L' {
+                    col &= 7 - (2 as u8).pow(x);
+                }
             }
-        }
+            col
+        });
+
+        let row = row_calc.join().unwrap();
+        let col = col_calc.join().unwrap();
+
         let ID: u32 = (row as u32 * 8) + col as u32;
         if ID > big {
             big = ID;
@@ -31,7 +45,7 @@ pub fn part_one(lines: &Vec<&str>) -> u32 {
     big
 }
 
-pub fn part_two(lines: &Vec<&str>) -> u32 {
+pub fn part_two(lines: &Vec<String>) -> u32 {
     let mut plane: [[bool; 8]; 128] = [[false; 8]; 128];
     for line in lines.iter() {
         let mut row: usize = 127;
